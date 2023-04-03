5c3bd813120416166eda9a71d49a4465f70bcda7
JovialGang <miroshnikov.egor@gmail.com>
2023-04-03 21:13:46 +0300

    maybe done

--- Task2/MakeOutPut.cpp
+++ Task2/MakeOutPut.cpp
@@ -5,25 +5,12 @@
 #include <string>
 #include <iostream>
 
-int countFiles(const std::filesystem::path& list){      // считаем общее количество файлов
-    static int files_count = 0;
-    for (auto file : std::filesystem::directory_iterator(list)){
-        if (file.is_directory()){
-            countFiles(file);
-        } else if (file.is_regular_file()){
-            files_count++;
-        }
-    }
-    return files_count;
-}
-
 
 void makeOutPutFle(const std::filesystem::path& list, const std::filesystem::path& outPutFile) {
 
     std::ofstream fout(outPutFile);   // выходной файл
     std::vector<std::filesystem::path> files;
-    files.resize(countFiles(list));     // задаём размер вектора
-    auto iter = tryParseComponent(list, files.begin());     // оттправляем в функцию итератор на первый элемент списка
+    tryParseComponent(list, files);
     sort(files.begin(), files.end());
     for (std::filesystem::path& path : files){
         fout << path.string() << std::endl;     // выводим в файл
@@ -31,14 +18,13 @@ void makeOutPutFle(const std::filesystem::path& list, const std::filesystem::pat
 }
 
 
-std::vector<std::filesystem::path>::iterator tryParseComponent(const std::filesystem::path& component, std::vector<std::filesystem::path>::iterator iter) {
+std::vector<std::filesystem::path> tryParseComponent(const std::filesystem::path& component, std::vector<std::filesystem::path>& files) {
     for (auto& anyPath: std::filesystem::directory_iterator(component)) {    // перебираем все компоненты в указанной директории
         if (anyPath.is_directory()) {    // если директория
-           iter = tryParseComponent(anyPath.path(), iter); // запускаемся от неё рекурсивно
+           files = tryParseComponent(anyPath.path(), files); // запускаемся от неё рекурсивно
         } else if (anyPath.is_regular_file()) {     // если файл, сохраняем
-           *iter = anyPath.path();  // разыменовываем указатель и записываем на этот адресс значение
-           iter++;      // передвигаем указатель на следующий элемент
+          files.push_back(anyPath.path());     // сохраняем
         }
     }
-    return iter;
+    return files;
 }
