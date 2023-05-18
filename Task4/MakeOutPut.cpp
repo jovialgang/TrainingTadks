2037cfcf41663f165bbbec56e09c9c123112c299
JovialGang <333412@niuitmo.ru>
2023-05-18 17:15:56 +0300

    4th task  done

--- Task4/MakeOutPut.cpp
+++ Task4/MakeOutPut.cpp
@@ -4,58 +4,44 @@
 #include <vector>
 #include <memory>
 #include <future>
-#include <shared_mutex>
 #include <iostream>
 
 
 void makeOutPutFle(const std::filesystem::path& list, const std::filesystem::path& outPutFile) {
 
-    std::shared_ptr<Params> params_ptr = std::make_shared<Params>();     // создаём умный указатель
+    std::vector<std::filesystem::path> allFiles;
 
-    // создаём два потока
-    params_ptr->FirstFuture = std::async(std::launch::async, findDirs, std::ref(list), params_ptr);
-    params_ptr->SecondFuture = std::async(std::launch::async, writeFiles, params_ptr);
-
-    params_ptr->FirstFuture.wait();     // ожидаем завершение первого потока
-    params_ptr->StopWriteFiles = false;     // останавливаем цикл while в writeFiles
-    params_ptr->Ready = true;
-    params_ptr->CV.notify_one();    // разблокируем поток, чтобы выйти из функции
-    params_ptr->SecondFuture.wait();    // ожидаем завершение второго потока
-
-
-
+    allFiles = parseComponent(list);
 
     std::ofstream fout(outPutFile);   // выходной файл
-    sort(params_ptr->Files.begin(), params_ptr->Files.end());
-    for (std::filesystem::path& path : params_ptr->Files){
+    std::sort(allFiles.begin(), allFiles.end());
+    for (std::filesystem::path& path : allFiles){
         fout << path.string() << std::endl;     // выводим в файл
     }
+
 }
 
-void writeFiles(const std::shared_ptr<Params>& ptr) {   // функция, которая считывает файлы
-    while (ptr->StopWriteFiles) {   // цикл, который мы потом остановим с помощью false
-        std::unique_lock<std::mutex> lock(ptr->Mtx);
-        ptr->CV.wait(lock, [&ptr] { return ptr->Ready; });  // останавливаем поток с помощью std::condition_variable
-        ptr->Ready = false;
-        for (auto &anyPath: std::filesystem::directory_iterator(    // стандартно перебираем компоненты
-                ptr->CurrentDir)) {    // работаем с каталогом, который получили в функции findDirs
-            if (anyPath.is_directory()) {
-                continue;
-            } else if (anyPath.is_regular_file()) {
-                ptr->Files.push_back(anyPath.path());
-            }
-        }
+std::vector<std::filesystem::path> parseComponent(const std::filesystem::path& component){
+    std::vector<std::filesystem::path> currentFiles;
+    std::vector<std::future<std::vector<std::filesystem::path>>> futures;
 
-    }
-}
-
-void findDirs(const std::filesystem::path& component, const std::shared_ptr<Params>& ptr){
-    for (auto& anyPath: std::filesystem::directory_iterator(component)) {       // запустить каждый каталог потом в отдельном пототке
+    for (auto& anyPath: std::filesystem::directory_iterator(component)) {
         if (anyPath.is_directory()) {
-            ptr->CurrentDir = anyPath;  // сохраняем текущий каталог
-            ptr->Ready = true;
-            ptr->CV.notify_one();   // разблокируем поток в функции writeFiles
-            findDirs(anyPath, ptr);
+            auto task = [anyPath]() {
+                return parseComponent(anyPath.path());
+            };
+            futures.push_back(std::async(std::launch::async, task));
+        } else if (anyPath.is_regular_file()){
+            currentFiles.push_back(anyPath);
         }
     }
+
+    for (auto & future : futures){
+        future.wait();
+        std::vector<std::filesystem::path> anyDir;
+        anyDir = future.get();
+        currentFiles.insert(currentFiles.end(), anyDir.begin(), anyDir.end());
+    }
+
+    return currentFiles;
 }
--- Task4/MakeOutPut.h
+++ Task4/MakeOutPut.h
@@ -4,20 +4,7 @@
 #include <shared_mutex>
 #include <future>
 
-
-struct Params {
-    std::vector<std::filesystem::path> Files;   // список файлов
-    std::mutex Mtx;
-    std::filesystem::path CurrentDir;   // текущая директория
-    std::condition_variable CV;
-    std::future<void> FirstFuture;
-    std::future<void> SecondFuture;
-    bool Ready = false;     // условие блокировки/освобождения потоков
-    bool StopWriteFiles = true;
-};
-
 void makeOutPutFle(const std::filesystem::path& list, const std::filesystem::path& outPutFile);
 
-void writeFiles(const std::shared_ptr<Params>& ptr);
+std::vector<std::filesystem::path> parseComponent(const std::filesystem::path& component);
 
-void findDirs(const std::filesystem::path& component, const std::shared_ptr<Params>& ptr);
